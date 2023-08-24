b98e647d9160055a9f3aeb7c201cfe8b79243fe2
Rob Hales <rob.hales@xelix.com>
2023-08-24 12:50:15 +0100

    13.2 Inheriting methods

--- src/LoxClass.java
+++ src/LoxClass.java
@@ -41,6 +41,10 @@ public class LoxClass implements LoxCallable {
             return methods.get(name);
         }
 
+        if (superclass != null) {
+            return superclass.findMethod(name);
+        }
+
         return null;
     }
 }
